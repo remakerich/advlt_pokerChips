395ad22499a9512989792bd7c9b1fbdc11eb8ef2
Konstantin Popovskikh <remakerich@mail.ru>
2021-06-23 10:32:15 +0600

    working version

--- PokerChips.py
+++ PokerChips.py
@@ -1,13 +1,3 @@
-data = [2, 7, 4, 2, 4, 10, 5, 7, 2, 7]
-print(f'{data}  initial')
-average = sum(data) / len(data)
-step = 0
-
-if average % 1 != 0:
-    print(f'Average = {average:.2f} is not a whole number\n')
-    exit()
-
-
 def indices(lst, element):
     result = []
     offset = - 1
@@ -19,61 +9,74 @@ def indices(lst, element):
         result.append(offset)
 
 
-while True:
-    MAX = max(data)
-    MIN = min(data)
+def process(data, average):
+    step = 0
+    while True:
+        maxvalue = max(data)
+        minvalue = min(data)
 
-    allMAXindices = indices(data, MAX)
-    allMINindices = indices(data, MIN)
+        maxindices = indices(data, maxvalue)
+        minindices = indices(data, minvalue)
 
-    # print(allMAXindices, allMINindices)
+        # print(allMAXindices, allMINindices)
 
-    fullset = []
-    for imax in allMAXindices:
-        for imin in allMINindices:
-            if imax > imin:
-                distanceLEFT = imax - imin
-                distanceRIGHT = len(data) - imax + imin
-            else:
-                distanceRIGHT = imin - imax
-                distanceLEFT = len(data) - imin + imax
+        fullset = []
+        for imax in maxindices:
+            for imin in minindices:
+                if imax > imin:
+                    distance_left = imax - imin
+                    distance_right = len(data) - imax + imin
+                else:
+                    distance_right = imin - imax
+                    distance_left = len(data) - imin + imax
 
-            # set of coordinates for every max value
-            # [index, left/right ditance, distance]
-            coord = [imax]
+                # set of coordinates for every max value
+                # [index, left/right ditance, distance]
+                coord = [imax]
 
-            if distanceLEFT < distanceRIGHT:
+                if distance_left < distance_right:
 
-                side = 'Left'
-                coord.append(side)
-                coord.append(distanceLEFT)
-            else:
-                side = 'Right'
-                coord.append(side)
-                coord.append(distanceRIGHT)
-            fullset.append(coord)
+                    side = 'Left'
+                    coord.append(side)
+                    coord.append(distance_left)
+                else:
+                    side = 'Right'
+                    coord.append(side)
+                    coord.append(distance_right)
+                fullset.append(coord)
 
-    # find shortest distance
-    tmp = fullset[0]
-    for crd in fullset:
-        if crd[2] < tmp[2]:
-            tmp = crd
-    # move a chip
-    indexOfGOODmax = tmp[0]
-    side = tmp[1]
+        # find shortest distance
+        tmp = fullset[0]
+        for crd in fullset:
+            if crd[2] < tmp[2]:
+                tmp = crd
+        # move a chip
+        index_of_target_max = tmp[0]
+        side = tmp[1]
 
-    if indexOfGOODmax == len(data) - 1:
-        indexOfGOODmax = -1
+        if index_of_target_max == len(data) - 1:
+            index_of_target_max = -1
 
-    if side == 'Right':
-        data[indexOfGOODmax + 1] += 1
-        data[indexOfGOODmax] -= 1
-    else:
-        data[indexOfGOODmax - 1] += 1
-        data[indexOfGOODmax] -= 1
+        if side == 'Right':
+            data[index_of_target_max + 1] += 1
+            data[index_of_target_max] -= 1
+        else:
+            data[index_of_target_max - 1] += 1
+            data[index_of_target_max] -= 1
 
-    step += 1
-    print(f'{data}, step: {step}')
-    if data.count(int(average)) == len(data):
-        print('DONE')
-        break
+        step += 1
+        print(f'{data}, step: {step}')
+        if data.count(int(average)) == len(data):
+            print('DONE')
+            break
+
+
+numbers = [13, 8, 28, 21, 30, 6, 13, 27, 23, 1]
+print(f'{numbers}  initial')
+avg = sum(numbers) / len(numbers)
+
+if avg % 1 != 0:
+    print(f'Average = {avg:.2f} is not a whole number\n')
+    exit()
+
+process(numbers, avg)
